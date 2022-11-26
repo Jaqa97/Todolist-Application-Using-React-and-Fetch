5ac372f3483b405e11fa7f13651306066bde191b
Jaqa97 <joaquinonesa@gmail.com>
2022-11-26 15:44:17 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+export default function App() {
+
+	const [lista, agregar] = useState([]);
+
+	useEffect(()=>{
+		fetch("http://assets.breatheco.de/apis/fake/todos/user/jaqa").then((respuesta) => respuesta.json()).then((data) => agregar(data));
+		console.log(lista)
+	}, []);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App">
+			<h1>To Do List!</h1>
+			<form 
+				onSubmit={(evento) => {
+					alert(evento.target[0].value);
+				}}
+			>
+
+				<input type="text" placeholder="Agregar Tarea"/>
+			</form>
+			{lista.map((elm, index)=>{
+				return (<li>{elm.index}</li>);
+			})}
+			
 		</div>
 	);
 };
 
-export default Home;
+
